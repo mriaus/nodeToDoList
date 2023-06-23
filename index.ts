fe7504672a0c576157fdc5635c57cf9d8ecdc878
Marcos Riau <riau222@gmail.com>
2023-06-23 17:38:43 +0200

    Changes in index and solved the problem with the notesMock file

--- index.ts
+++ index.ts
@@ -1,15 +1,5 @@
 const express = require('express')
-const notes = require('./src/data/notesMock.ts')
-
-//const http = require('http')
-
-
-/*  PAra hacerlo con el require('http')
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-type': 'application/json'}) //Cabezera, respuesta 200 si es correcto y respuesta de tipo plano
-    response.end(JSON.stringify(notes))
-})
-*/
+const notes = require('./src/data/notesMock.json')
 
 const app = express()
 
@@ -19,33 +9,33 @@ let port = process.env.PORT
 app.listen(port)
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hola bb</h1>')
+    response.send('<h1>SERVER UP</h1>')
 })
 
 app.get('/notes', (request, response) => {
     response.json(notes)
 })
 app.get('/notes/:id', (request, response) => {
-  const note = notes.find(item => item.id === Number(request.params.id))
+    const note = notes.find(item => item.id === Number(request.params.id))
 
-if(note){
-    response.json(notes.find(item => item.id === Number(request.params.id)))
+    if (note) {
+        response.json(notes.find(item => item.id === Number(request.params.id)))
 
-}else{
-    response.status('404')
-}
+    } else {
+        response.status('404')
+    }
 })
 
 app.delete('/notes/:id', (request, response) => {
     const note = notes.filter(item => item.id !== Number(request.params.id))
-  
-  if(note){
-      response.json(note)
-  
-  }else{
-      response.status('204').end()
-  }
-  })
+
+    if (note) {
+        response.json(note)
+
+    } else {
+        response.status('204').end()
+    }
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`SERVER RUNING ON PORT ${PORT}`)
